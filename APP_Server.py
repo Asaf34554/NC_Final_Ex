3fe59c74a5ea1a26927221cabfdf469e5b160d0d
Asaf34554 <Asaf34554@gmail.com>
2023-03-12 21:30:49 +0200

    mid_app

--- APP_Server.py
+++ APP_Server.py
@@ -1,17 +1,35 @@
 import socket
+import time
+
 import requests
 from scapy.all import *
 import os
 from PIL import Image,ImageSequence
-from scapy.layers.inet import IP, TCP
+from scapy.layers.inet import IP, TCP, UDP
 
-RUDP_MAX_SIZE = 64000
-SERVER_ADDRESS = ("127.0.0.2",30493)
+RUDP_MAX_SIZE = 62000
+SERVER_ADDRESS = ("127.0.0.10",30493)
+
+
+class RUDP(Packet):
+    name = "RUDP"
+    fields_desc = [
+        ShortField("src_port", 80),
+        ShortField("dst_port", 80),
+        IntField("seq_num", 0),
+        IntField("ack_num", 0),
+        FlagsField("flags", 0, 8, ['S', 'A', 'F']),
+        ShortField("window", 8192),
+        ShortField("checksum", None),
+        ShortField("urgent_ptr", 0),
+        StrField("payload", "")
+    ]
+
 
 memes = [
     ('https://speech.protocommunications.com/wp-content/uploads/2018/03/frustrated-meme.png','frustrated_meme.png'),
     ('https://media.wired.com/photos/5f87340d114b38fa1f8339f9/master/w_1600,c_limit/Ideas_Surprised_Pikachu_HD.jpg','surprised_meme.png'),
-    ('https://www.happierhuman.com/wp-content/uploads/2022/06/anger-meme-littlenivi-come-here-you.jpg','angry_meme.png'),
+    ('https://m.media-amazon.com/images/I/61k7AtBBAVS._AC_SL1327_.jpg','angry_meme.png'),
     ('https://i.imgflip.com/639z7.jpg','stoned_meme.png'),
     ('https://cache.lovethispic.com/uploaded_images/blogs/10-Happy-Memes-To-Help-Your-Day-Feel-A-Whole-Lot-Better-50193-3.jpg','happy_meme.png'),
     ('https://scontent.ftlv19-1.fna.fbcdn.net/v/t1.6435-9/46310761_1843365659095168_484399811940843520_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=MPsSPM_c_g0AX8dRueG&_nc_ht=scontent.ftlv19-1.fna&oh=00_AfB0gYkK9MEn3wqhISclfum7Kk-LZrhA-X9QhUnTW8ibng&oe=64340756','bored_meme.png'),
@@ -19,25 +37,13 @@ memes = [
 ]
 
 
-
-# def get_Object():
-#     our_giff = requests.get(url)
-#     if  our_giff.status_code == 200:
-#         with open("wb") as giff:
-#             giff.write(our_giff.content)
-#             gif = Image.open('/home/assaf/PycharmProjects/FinalEx/Object.gif')
-#
-#     else:
-#         print("failed to download the gif")
-
-
 def tcp_listen():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(SERVER_ADDRESS)
-    while(True):
-        server_socket.listen()
-        client_socket , address = server_socket.accept()
-        tcp_connection(client_socket , address)
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_server_socket.bind(SERVER_ADDRESS)
+    while True:
+        tcp_server_socket.listen()
+        client_socket , client_address = tcp_server_socket.accept()
+        tcp_connection(client_socket , client_address)
 
 
 def tcp_connection(client_socket,address):
@@ -53,6 +59,94 @@ def tcp_connection(client_socket,address):
     # client_socket.close()
 
 
+def get_image(meme_num):
+    return requests.get(memes[int(meme_num)][0])
+
+    # if our_meme.status_code == 200:
+    #     image = Image.open('/home/assaf/PycharmProjects/FinalEx/Object.g')
+    #      return image
+    # else:
+    #     print("failed to download the image")
+
+
+def rudp_connection():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(SERVER_ADDRESS)
+    sock.settimeout(1.5)
+
+    # Wait for SYN packet from client
+    syn_received = False
+    pkt = RUDP()
+    client_address = None
+    while not syn_received:
+        time.sleep(0.3)
+        data, addr = sock.recvfrom(RUDP_MAX_SIZE)
+        pkt = RUDP(data)
+        if pkt.flags == 'S':
+            syn_received = True
+            client_address = addr
+
+
+    # Send SYN-ACK packet to client
+    seq_num = 0
+    ack_num = pkt.seq_num + 1
+    syn_ack_packet = RUDP(dst_port=pkt.src_port,src_port=SERVER_ADDRESS[1], flags=['S','A'], seq_num=seq_num, ack_num=ack_num)
+    time.sleep(0.2)
+    sock.sendto(bytes(syn_ack_packet),client_address)
+    print(syn_ack_packet.show())
+    # Wait for ACK packet from client
+    ack_image_received = False
+    while not ack_image_received:
+        # time.sleep(0.1)
+        data, addr = sock.recvfrom(RUDP_MAX_SIZE)
+        pkt = RUDP(data)
+        if pkt.flags == 'A' :
+            ack_num += len(data)
+            ack_image_received = True
+    seq_num = pkt.ack_num
+    image_to_send = get_image(pkt.payload)
+    img_size = len(image_to_send.content)
+    chunk_size = RUDP_MAX_SIZE - 4000
+    start_idx = 0
+    end_idx = chunk_size
+    while start_idx < img_size:
+        # Send packet with image data
+        pkt = RUDP(dst_port=pkt.dst_port, seq_num=start_idx+seq_num, ack_num=ack_num, payload=image_to_send[start_idx:end_idx])
+        sock.sendto(bytes(pkt),client_address)
+
+        # Wait for ACK packet from client
+        ack_received = False
+        while not ack_received:
+            data, addr = sock.recvfrom(RUDP_MAX_SIZE)
+            pkt = RUDP(data)
+            if pkt.flags == 'A' and pkt.ack_num == end_idx:
+                ack_received = True
+
+        # Update indexes for next chunk
+        start_idx = end_idx
+        end_idx += chunk_size
+
+    # Wait for FIN request packet from client
+    fin_requested = False
+    while not fin_requested:
+        data, addr = sock.recvfrom(RUDP_MAX_SIZE)
+        pkt = RUDP(data)
+        if pkt.flags == "F":
+            fin_requested = True
+
+    # Send FIN packet to client
+    fin_packet = RUDP(dst_port=client_address[1], flags="FA", seq_num=start_idx, ack_num=ack_num)
+    sock.sendto(bytes(fin_packet),client_address)
+    fin_ack = False
+    while not fin_ack:
+        data, addr = sock.recvfrom(RUDP_MAX_SIZE)
+        pkt = RUDP(data)
+        if pkt.flags == 'A':
+            fin_received = True
+
+
 if __name__ == '__main__':
-        tcp_listen()
+    # tcp_listen()
+    rudp_connection()
+
 
--- client2.py
+++ client2.py
@@ -1,13 +1,100 @@
-import random
+import time
+
+from lxml.sax import SaxError
+# from termcolor import colored
 from io import BytesIO
 import socket
 import requests
 from scapy.all import *
 from scapy.layers.inet import IP, TCP
-from PIL import Image,ImageSequence
+from PIL import Image
 
-CLIENT_ADDRESS = ('127.0.0.20',20454)
-SERVER_ADDRESS = ("127.0.0.2",30493)
+CLIENT_ADDRESS = ('127.0.0.2',20454)
+SERVER_ADDRESS = ("127.0.0.10",30493)
+RUDP_MAX_SIZE = 62000
+
+
+class RUDP(Packet):
+    name = "RUDP"
+    fields_desc = [
+        ShortField("src_port", 80),
+        ShortField("dst_port", 80),
+        IntField("seq_num", 0),
+        IntField("ack_num", 0),
+        FlagsField("flags", 0, 8, ['S', 'A', 'F']),
+        ShortField("window", 8192),
+        ShortField("checksum", None),
+        ShortField("urgent_ptr", 0),
+        StrField("payload1", "")
+    ]
+
+
+def rudp_connecet(msg_num):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(CLIENT_ADDRESS)
+    # sock.settimeout(1.5)
+    pkt = RUDP(src_port = CLIENT_ADDRESS[1],flags = "S",seq_num = 0)
+    sock.sendto(bytes(pkt),SERVER_ADDRESS)
+    ack_syn_received = False
+    ack_num=0
+    while not ack_syn_received:
+        # time.sleep(0.2)
+        data, addr = sock.recvfrom(RUDP_MAX_SIZE)
+        pkt = RUDP(data)
+        if pkt.flags == ['S','A'] and pkt.seq_num == 0:
+            ack_num += 1
+            ack_syn_received = True
+    print(pkt.show())
+
+    pkt_new = RUDP(payload1='1',seq_num=pkt.seq_num + 1, ack_num=ack_num, dst_port=pkt.src_port, flags='A')
+    print(pkt_new.show())
+    sock.sendto(bytes(pkt_new),SERVER_ADDRESS)
+    image_bytes = io.BytesIO()
+    expected_seq_num = 1
+    while True:
+        # Wait for packet from server
+        # time.sleep(0.3)
+        data, addr = sock.recvfrom(RUDP_MAX_SIZE)
+        pkt = RUDP(data)
+
+        # Check packet sequence number
+        if pkt.seq_num == expected_seq_num:
+            # Write payload to image buffer
+            image_bytes.write(pkt.payload)
+            expected_seq_num += len(pkt.payload)
+
+            # Send ACK packet to server
+            ack_packet = RUDP(dst_port=pkt.src_port, flags="A", seq_num=pkt.ack_num, ack_num=expected_seq_num)
+            sock.sendto(bytes(pkt), SERVER_ADDRESS)
+
+            # Check for end of image
+            if len(pkt.payload) < RUDP_MAX_SIZE - 4000:
+                break
+        else:
+            # Send ACK packet to server for last correctly received packet
+            ack_packet = RUDP(dst_port=pkt.src_port, flags="A", seq_num=pkt.ack_num, ack_num=expected_seq_num)
+            sock.sendto(bytes(ack_packet), SERVER_ADDRESS)
+
+    ack_packet = RUDP(dst_port=pkt.src_port, flags="A", seq_num=pkt.seq_num + len(pkt.payload),ack_num=expected_seq_num)
+    sock.sendto(bytes(ack_packet), SERVER_ADDRESS)
+
+    fin_request_packet = RUDP(dst_port=pkt.src_port, flags="F", seq_num=pkt.seq_num + len(pkt.payload),ack_num=expected_seq_num)
+    sock.sendto(bytes(fin_request_packet), SERVER_ADDRESS)
+
+    fin_received = False
+    while not fin_received:
+        data, addr = sock.recvfrom(RUDP_MAX_SIZE)
+        pkt = RUDP(data)
+        if pkt.flags == 'FA':
+            fin_received = True
+
+    # Send ACK packet to server
+    ack_packet = RUDP(dst_port=pkt.src_port, flags="A", seq_num=pkt.ack_num, ack_num=pkt.seq_num + 1)
+    sock.sendto(bytes(ack_packet), SERVER_ADDRESS)
+
+    # Return image data as bytes
+    return image_bytes.getvalue()
+
 
 
 def get_meme(url):
@@ -17,14 +104,13 @@ def get_meme(url):
     image.show()
 
 
-def connect_tcp():
+def connect_tcp(msg_num):
     client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     client_socket.bind(CLIENT_ADDRESS)
     client_socket.connect(SERVER_ADDRESS)
-    # message = str(random.randint(0,6))
-    message = "2"
-    client_socket.sendall(message.encode('utf-8'))
-    print("After sending the request url")
+
+    client_socket.sendall(msg_num.encode('utf-8'))
+    # print("After sending the request url")
     url_data = b''
 
     url_data += client_socket.recv(1048)
@@ -34,8 +120,31 @@ def connect_tcp():
 
     client_socket.close()
 
-# def redirect():
-#
 
 if __name__ == '__main__':
-    connect_tcp()
+
+    img_bytes=rudp_connecet(2)
+    img = Image.open(io.BytesIO(img_bytes))
+    img.show()
+    # print(colored("Welcome to the Meme Generator App","yellow","on_black",attrs=["bold","underline"]))
+    #
+    # proto=input(colored("Choose the Protocol type you want to communicate with:\n<1>:TCP.\n<2>:RUDP.\n","red","on_black",attrs=["bold","underline"]))
+    # print(proto)
+    # if proto == '1' or proto.upper() == "TCP":
+    #     opt=input(colored("Do you want to choose a meme by yourself or for we to generate a random meme for you?:\n<1>:Choose from library.\n<2>:Surprise me!.\n","red","on_black",attrs=["bold","underline"]))
+    #     if opt=='1':
+    #         meme_opt = input("Choose the number of your choice from the Meme-Library:\n<1>:Frustrated Meme\n<2>:Surprised Meme\n<3>:Angry Meme\n<4>:'Stoned' Meme\n<5>:Happy Meme\n<6>:Bored Meme\n<7>:Nerd Meme\n")
+    #         meme_opt=str((int(meme_opt)-1))
+    #         connect_tcp(meme_opt)
+    #     elif opt=='2':
+    #         meme_opt = str(random.randint(0,6))
+    #         connect_tcp(meme_opt)
+    #
+    # elif proto == '1' or proto.upper() == "RUDP":
+    #     opt=input(colored("Do you want to choose a meme by yourself or for we to generate a random meme for you?:\n<1>:Choose from library.\n<2>:Surprise me!.\n","red","on_black",attrs=["bold","underline"]))
+    #     if opt=='1':
+    #         meme_opt = input("Choose the number of your choice from the Meme-Library:\n<1>:Frustrated Meme\n<2>:Surprised Meme\n<3>:Angry Meme\n<4>:'Stoned' Meme\n<5>:Happy Meme\n<6>:Bored Meme\n<7>:Nerd Meme\n")
+    #     elif opt=='2':
+    #         meme_opt = str(random.randint(0,6))
+    # else:
+    #     print(colored("Input Error, restarting App!","black","on_yellow",attrs=["bold","underline"]))
